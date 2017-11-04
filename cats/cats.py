b5ba82c3a82e981de726cd2aa632f8dcf00bb975
Anamari Oštarijaš <anamari.ostarijas@gmail.com>
2017-11-04 20:21:08 +0100

    edited regular expression

--- cats/cats.py
+++ cats/cats.py
@@ -1,4 +1,6 @@
-from catlib import *
+#from catlib import *
 
-frontpage_contents = read_file_to_string(cat_dirname, frontpage_filename)
+download_frontpage_to_file()
+frontpage_contents = read_file_to_string(cats_dirname, cats_frontpage_basename)
 ads = split_into_ads(frontpage_contents)
+all_data = data_from_file(cats_dirname, cats_frontpage_basename)
--- cats/save_page_to_file.py
+++ cats/save_page_to_file.py
@@ -3,10 +3,10 @@ import re
 import string
 import requests
 import csv
-cats_frontpage_basename = 'macke'
+cats_frontpage_basename = 'macke.html'
 cats_frontpage_url = 'http://www.bolha.com/zivali/male-zivali/macke/'
 base_url = 'http://www.bolha.com'
-cats_dirname ='U:\programiranje 1'
+cats_dirname ='C:\Anamari\Documents\prog1_vaje'
 cats_frontpage_fn = os.path.join(cats_dirname, cats_frontpage_basename)
 csv_filename = 'cats.csv'
 
@@ -34,7 +34,7 @@ def save_string_to_file(text, directory, filename):
     return None
 
 def download_frontpage_to_file():
-    url_text = download_url_to_string(cats_frontpage_basename)
+    url_text = download_url_to_string(cats_frontpage_url)
     file = save_string_to_file(url_text, cats_dirname, cats_frontpage_fn )
 
     
@@ -53,18 +53,43 @@ def read_file_to_string(directory, filename):
 def split_into_ads(page_contents):
     '''splits page into segments'''
     rx = re.compile(r'<div class="ad">(.*?)<div class="clear">', re.DOTALL)
-    ads = rx.findall(page)
-    split_file = file_in.split(rx)
+    ads = rx.findall(page_contents)
+    split_file = page_contents.split(ads)
     return ads
 
-#def dl_cats_frontpage():
- #   save_page_to_file(cats_frontpage_url, cats_frontpage_fn)
+# Define a function that takes a string corresponding to the block of one
+# advertisement and extracts from it the following data: Name, price, and
+# the description as displayed on the page.
+def undefined( TODO ):
+    '''TODO'''
+    TODO
 
 
-def write_csv(dicts, fieldnames, fn):
-    with open(fn, 'w') as csv_dat:
-        writer = csv.DictWriter(csv_dat, fieldnames = fieldnames)
+
+# Write a function that reads a page from a file and returns the list of
+# dictionaries containing the information for each ad on that page.
+def undefined( TODO ):
+    '''TODO'''
+    TODO
+
+
+
+########################################################################
+# We processed the data, now let's save it for later.
+########################################################################
+
+def write_csv(fieldnames, rows, directory, filename):
+    '''Write a CSV file to directory/filename. The fieldnames must be a list of
+    strings, the rows a list of dictionaries each mapping a fieldname to a
+    cell-value.
+    '''
+    os.makedirs(directory, exist_ok=True)
+    path = os.path.join(directory, filename)
+    with open(path, 'w') as csv_file:
+        writer = csv.DictWriter(csv_file, fieldnames=fieldnames)
         writer.writeheader()
-        for d in dicts:
-            writer.writerow(d)
-    return
+        for row in rows:
+            writer.writerow(row)
+    return None
+
+
